a3cfcfd18a6ea8671a6a9eab3951d17f28009cf4
cdaringe <cdieringer@mrn.org>
2016-02-03 12:19:17 -0700

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# coins-validate
+
+COINS implementation of nfl's [git-validate](https://github.com/nlf/git-validate)
+
+applies coins formatting and pre-commit rules to packages.
+
+# usage
+
+`npm i --save coins-validate@latest`
+
+always use latest to assert that coins repos always follow our latest spec
---
+++ index.js
@@ -0,0 +1,10 @@
+var validate = require('git-validate');
+validate.copy('templates/.jshintrc', '.jshintrc');
+validate.copy('templates/.jscscrc', '.jscsrc');
+validate.installScript('lint', 'jscs .');
+validate.installScript('postlint', 'jshint .');
+validate.installScript('validate', 'npm ls');
+validate.installScript('preversion', 'git checkout master && git pull && npm ls');
+validate.installScript('publish-patch', 'npm run preversion && npm version patch && git push origin master --tags && npm publish');
+validate.installScript('publish-minor', 'npm run preversion && npm version minor && git push origin master --tags && npm publish');
+validate.installScript('publish-major', 'npm run preversion && npm version major && git push origin master --tags && npm publish');
