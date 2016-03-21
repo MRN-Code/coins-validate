8e680a612355937ff25d35f138274771a501ed72
cdieringer3 <cdieringer3@gatech.edu>
2016-03-21 13:12:03 -0600

    stringify airbnb

--- templates/.eslintrc.js
+++ templates/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   ecmaFeatures: {
     modules: false, // https://github.com/eslint/eslint/issues/2785#issuecomment-113254153
   },
-  extends: airbnb,
+  extends: 'airbnb',
   env: {
     node: true,
     commonjs: true,
